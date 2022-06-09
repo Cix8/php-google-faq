34cec786ca0ecaa8a209e539ada90af1968aa4cb
Cix8 <francesco.natal_fn@libero.it>
2022-06-09 13:50:35 +0200

    Init; Add header_structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="top">
+            <div class="left-col">
+                <div class="logo-container"></div>
+                <div class="page-title"></div>
+            </div>
+            <div class="right-col">
+                <div class="icon-container"></div>
+                <div class="avatar-container"></div>
+            </div>
+        </div>
+        <nav class="bottom">
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+
+</html>
